ba1b4d6a63173eafa8625f422c54a8ceb69aa9d1
Urroke <kovalevyura1@gmail.com>
2019-04-12 07:57:54 +0600

    final

--- ising/det_crit_point.cpp
+++ ising/det_crit_point.cpp
@@ -24,13 +24,13 @@ int main()
     constexpr size_t temps = 50;
     constexpr size_t size = 2000;
     constexpr size_t config = 100;
-    constexpr size_t L = 512;
+    constexpr size_t L = 128;
     size_t found = 0;
     constexpr double T1 = 2.26;
     constexpr double T2 = 2.28;
     double m_4, m_2, m, r;
     std::ofstream fout("1Aresults_" + std::to_string(L) + ".dat");
-    fout<<"T\tKUM\tM\tHI\n";
+    fout<<"T\tKUM\tERR_KUM\tM\tERR_M\tHI\tERR_HI\n";
     std::string dir = "newres/";
     std::vector<std::string> files = std::vector<std::string>();
 
@@ -52,16 +52,61 @@ int main()
                 m_4 += r*r*r*r;
                 m_2 += r*r;
                 m += r;
-                found ++;
             }
+            found ++;
+            fin.close();
         }
 
         std::cout<<T<<"\n";
-        m /= found;
-        m_2 /= found;
-        m_4 /= found;
-        fout<<(T*(T2 - T1)/temps + T1)<<"\t"<<0.5*(3 - m_4/std::pow(m_2, 2))
-        <<"\t"<<m/(L*L)<<"\t"<<(m_2 -
-        std::pow(m, 2))/(T*(T2 - T1)/temps + T1)<<"\n";
+        m /= found*size;
+        m_2 /= found*size;
+        m_4 /= found*size;
+
+        double kum_g = 0.5*(3 - m_4/std::pow(m_2, 2));
+        double hi_g = (m_2 - std::pow(m, 2))/(T*(T2 - T1)/temps + T1);
+        double m_g = m/(L*L);
+        double err_kum = 0;
+        double err_hi = 0;
+        double err_m = 0;
+
+        m *= 0;
+        m_2 *= 0;
+        m_4 *= 0;
+
+        for (unsigned int i = 0;i < files.size();i++)
+        if(files[i].size() > 7)
+        if(std::atoi(files[i].substr(27, 3).c_str()) == L)
+        if(std::atoi(files[i].substr(31, 2).c_str()) == T){
+            std::ifstream fin(dir + files[i]);
+            for(int k = 0;k < size;k++){
+                fin>>r;
+                m_4 += r*r*r*r;
+                m_2 += r*r;
+                m += r;
+            }
+            m /= size;
+            m_2 /= size;
+            m_4 /= size;
+
+            double kum = 0.5*(3 - m_4/std::pow(m_2, 2));
+            double hi = (m_2 - std::pow(m, 2))/(T*(T2 - T1)/temps + T1);
+            double m_ = m/(L*L);
+
+            err_kum += std::pow(kum - kum_g, 2);
+            err_m += std::pow(m_ - m_g, 2);
+            err_hi += std::pow(hi - hi_g, 2);
+
+            fin.close();
+        }
+
+        err_kum /= found;
+        err_m /= found;
+        err_hi /= found;
+
+        err_kum = std::pow(err_kum, 0.5);
+        err_m = std::pow(err_m, 0.5);
+        err_hi = std::pow(err_hi, 0.5);
+
+        fout<<(T*(T2 - T1)/temps + T1)<<"\t"<<kum_g<<"\t"<<err_kum<<"\t"<<m_g<<"\t"<<err_m<<"\t"<<hi_g<<"\t"<<err_hi<<"\n";
     }
 }
